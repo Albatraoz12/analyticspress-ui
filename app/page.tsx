aa94aaf057975b511e1445afa837eb2315bc9823
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-06-13 22:07:22 +0200

    make card display dynamic data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import DashboardCard from '@/components/dashboard/DashboardCard';
+import { Newspaper } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
       <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
-        <DashboardCard />
+        <DashboardCard
+          title='Posts'
+          count={100}
+          icon={<Newspaper className='text-slate-500' size={72} />}
+        />
       </div>
     </>
   );
