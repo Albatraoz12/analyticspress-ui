9b48574116a9585245b67ca3f3f9ecdf6368a3ed
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-06-14 09:03:00 +0200

    add toast to inform user that post has been updated

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -16,6 +16,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import posts from '@/data/posts';
+import { useToast } from '@/components/ui/use-toast';
 
 const formSchema = z.object({
   title: z.string().min(1, {
@@ -39,6 +40,7 @@ interface PostsEditPageProps {
 }
 
 const PostsEditPage = ({ params }: PostsEditPageProps) => {
+  const { toast } = useToast();
   const post = posts.find((post) => post.id === params.id);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -52,7 +54,10 @@ const PostsEditPage = ({ params }: PostsEditPageProps) => {
   });
 
   const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data);
+    toast({
+      title: 'Post has been updated successfully',
+      description: `Updated by ${post?.author} on ${post?.date}`,
+    });
   };
 
   return (
