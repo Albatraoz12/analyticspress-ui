1955f961cb587c41ffdff01f12ad6ce5cf1bd0d9
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-06-17 22:49:07 +0300

    add headers to authtabs

--- components/auth/AuthTabs.tsx
+++ components/auth/AuthTabs.tsx
@@ -2,15 +2,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const AuthTabs = () => {
   return (
-    <Tabs defaultValue='account' className='w-[400px]'>
-      <TabsList>
-        <TabsTrigger value='account'>Account</TabsTrigger>
-        <TabsTrigger value='password'>Password</TabsTrigger>
+    <Tabs defaultValue='login' className='w-[400px]'>
+      <TabsList className='grid w-full grid-cols-2'>
+        <TabsTrigger value='login'>Login</TabsTrigger>
+        <TabsTrigger value='register'>Register</TabsTrigger>
       </TabsList>
-      <TabsContent value='account'>
-        Make changes to your account here.
-      </TabsContent>
-      <TabsContent value='password'>Change your password here.</TabsContent>
+      <TabsContent value='login'>LOGIN HERE</TabsContent>
+      <TabsContent value='register'>REGISTER HERE</TabsContent>
     </Tabs>
   );
 };
